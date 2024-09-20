d1a4b99464f8c201146e898fc8b472950eaa7fc4
typicalHuman <ruslanzhezh@zohomail.eu>
2024-09-20 13:57:03 +0500

    m

--- test/playwright/03_advanced.spec.ts
+++ test/playwright/03_advanced.spec.ts
@@ -1,60 +1,60 @@
-import { test as advancedFixtures } from './advancedFixtures'
+// import { test as advancedFixtures } from './advancedFixtures'
 
-import { testWithSynpress } from '@synthetixio/synpress'
+// import { testWithSynpress } from '@synthetixio/synpress'
 
-const test = testWithSynpress(advancedFixtures)
+// const test = testWithSynpress(advancedFixtures)
 
-const { expect, describe } = test
+// const { expect, describe } = test
 
-const tokenLocator = '.multichain-token-list-item'
+// const tokenLocator = '.multichain-token-list-item'
 
-describe('Token', () => {
-  test('should confirm tokens transfer', async ({ page, metamask, connectToAnvil, deployToken }) => {
-    await connectToAnvil()
-    await deployToken()
+// describe('Token', () => {
+//   test('should confirm tokens transfer', async ({ page, metamask, connectToAnvil, deployToken }) => {
+//     await connectToAnvil()
+//     await deployToken()
 
-    await page.locator('#transferTokens').click()
-    await metamask.confirmTransaction()
-  })
+//     await page.locator('#transferTokens').click()
+//     await metamask.confirmTransaction()
+//   })
 
-  test('should approve tokens', async ({ page, metamask, connectToAnvil, deployToken }) => {
-    await connectToAnvil()
-    await deployToken()
+//   test('should approve tokens', async ({ page, metamask, connectToAnvil, deployToken }) => {
+//     await connectToAnvil()
+//     await deployToken()
 
-    await page.locator('#approveTokens').click()
-    await metamask.approveTokenPermission()
-  })
+//     await page.locator('#approveTokens').click()
+//     await metamask.approveTokenPermission()
+//   })
 
-  test('should add token to the wallet', async ({ page, metamask, metamaskPage, connectToAnvil, deployToken }) => {
-    await connectToAnvil()
-    await deployToken()
+//   test('should add token to the wallet', async ({ page, metamask, metamaskPage, connectToAnvil, deployToken }) => {
+//     await connectToAnvil()
+//     await deployToken()
 
-    await page.locator('#watchAssets').click()
+//     await page.locator('#watchAssets').click()
 
-    await metamask.addNewToken()
+//     await metamask.addNewToken()
 
-    await expect(metamaskPage.locator(tokenLocator).nth(1)).toContainText('TST')
-  })
-})
+//     await expect(metamaskPage.locator(tokenLocator).nth(1)).toContainText('TST')
+//   })
+// })
 
-describe('Piggy Bank', () => {
-  test('should deposit and then withdraw ETH from the contract', async ({
-    page,
-    metamask,
-    connectToAnvil,
-    deployPiggyBank
-  }) => {
-    await connectToAnvil()
-    await deployPiggyBank()
+// describe('Piggy Bank', () => {
+//   test('should deposit and then withdraw ETH from the contract', async ({
+//     page,
+//     metamask,
+//     connectToAnvil,
+//     deployPiggyBank
+//   }) => {
+//     await connectToAnvil()
+//     await deployPiggyBank()
 
-    await page.locator('#depositButton').click()
-    await metamask.confirmTransaction()
+//     await page.locator('#depositButton').click()
+//     await metamask.confirmTransaction()
 
-    await expect(page.locator('#contractStatus')).toHaveText('Deposit completed')
+//     await expect(page.locator('#contractStatus')).toHaveText('Deposit completed')
 
-    await page.locator('#withdrawButton').click()
-    await metamask.confirmTransaction()
+//     await page.locator('#withdrawButton').click()
+//     await metamask.confirmTransaction()
 
-    await expect(page.locator('#contractStatus')).toHaveText('Withdrawn')
-  })
-})
+//     await expect(page.locator('#contractStatus')).toHaveText('Withdrawn')
+//   })
+// })
